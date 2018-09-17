d2c24b35510e247d108da79d2af9d3d37a57fafb
CoelyPuffs <turquoisstar@yahoo.com>
2018-09-17 09:48:23 -0400

    Caf page parses well, working on vm page

--- SouthernCuisine/SouthernCuisine/AllMenusPage.xaml.cs
+++ SouthernCuisine/SouthernCuisine/AllMenusPage.xaml.cs
@@ -33,7 +33,7 @@ namespace SouthernCuisine
             int dayEndIndex = fullMenu.IndexOf("</td>", dayStartIndex);
             string dayMenu = fullMenu.Substring(dayStartIndex, dayEndIndex - dayStartIndex);
 
-            string[] meals = { "Breakfast", "Grab n Go", "Lunch", "International Bar", "Grab n Go", "Supper" };
+            string[] meals = { "Breakfast", /*"Grab n Go",*/ "Lunch", "International Bar", /*"Grab n Go",*/ "Supper" };
 
             string displayMenu = "";
             int mealStartIndex = 0;
@@ -53,7 +53,7 @@ namespace SouthernCuisine
 
             displayMenu = displayMenu.Replace("&amp;", "&");
             displayMenu = displayMenu.Replace("<br>", ", ");
-            displayMenu = Regex.Replace(displayMenu, "[<].[^<>].[>]", " ");
+            displayMenu = Regex.Replace(displayMenu, "<[^<>]*>", "");
 
             if (displayMenu != "")
             {
@@ -67,7 +67,51 @@ namespace SouthernCuisine
 
         private void VMMenuButton_Clicked(object sender, EventArgs e)
         {
+            Label menuLabel = Content.FindByName<Label>("MenuLabel");
+            WebClient client = new WebClient();
+            string fullMenu = client.DownloadString("http://www.southern.edu/administration/food/deli.html");
+            string dayToday = DateTime.Now.DayOfWeek.ToString();
+            if (dayToday == "Saturday")
+            {
+                dayToday = "Sabbath";
+            }
+            int dayStartIndex = fullMenu.IndexOf(dayToday);
+            int dayEndIndex = fullMenu.IndexOf("</div>", dayStartIndex);
+            string dayMenu = fullMenu.Substring(dayStartIndex, dayEndIndex - dayStartIndex);
 
+            string[] meals = { "Breakfast", "Lunch", "Soup", "Supper" };
+
+            string displayMenu = "";
+            int mealStartIndex = 0;
+            int mealEndIndex = 0;
+            foreach (string meal in meals)
+            {
+                if (dayMenu.IndexOf(meal) != -1)
+                {
+                    mealStartIndex = dayMenu.IndexOf(meal);
+                    mealStartIndex = dayMenu.IndexOf("m<", mealStartIndex) + 1;
+                    mealStartIndex = findEndOfHTMLTags(dayMenu, mealStartIndex);
+                    mealEndIndex = dayMenu.IndexOf("</ul>", mealStartIndex);
+
+                    displayMenu += meal + '\n' + dayMenu.Substring(mealStartIndex, mealEndIndex - mealStartIndex) + '\n';
+                }
+            }
+
+            displayMenu = displayMenu.Replace('\n', ' ');
+            displayMenu = displayMenu.Replace("&amp;", "&");
+            displayMenu = displayMenu.Replace("&nbsp;", "");
+            displayMenu = displayMenu.Replace("<br>", ", ");
+            displayMenu = displayMenu.Replace("                                                ", " ");
+            displayMenu = Regex.Replace(displayMenu, "<[^<>]*>", "");
+
+            if (displayMenu != "")
+            {
+                menuLabel.Text = displayMenu;
+            }
+            else
+            {
+                menuLabel.Text = "No food today";
+            }
         }
 
         public int findEndOfHTMLTags(string menu, int startIndex)
